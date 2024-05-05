b546741ec1086cda445282691ce83308c69705f2
I0I-I0I <azudjndj@gmail.com>
2024-05-05 20:27:32 +0300

    +

--- lua/mini/init.lua
+++ lua/mini/init.lua
@@ -1,6 +1,8 @@
-require("mini.config")
-require("mini.mappings")
-require("mini.plug")
+local core = "mini.core."
+
+require(core .. "config")
+require(core .. "mappings")
+require(core .. "plug")
 require("mini.lsp")
 
 vim.cmd("colorscheme tokyonight-moon")
