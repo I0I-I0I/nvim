5eacb1d41f36573f161ffb87a90f8982c054597b
I0I-I0I <azudjndj@gmail.com>
2024-08-08 00:12:30 +0300

    Fixed zenmode behavior on open

--- plugins/zenmode/plugin/zenmode.lua
+++ plugins/zenmode/plugin/zenmode.lua
@@ -5,6 +5,7 @@ vim.api.nvim_create_user_command("ZenmodeToggle", function(c)
 end, { nargs = "?" })
 
 vim.api.nvim_create_user_command("ZenmodeOpen", function(c)
+	zenmode.zenmode_close()
 	zenmode.zenmode_open(c.fargs[1])
 end, { nargs = "?" })
 
@@ -17,5 +18,6 @@ vim.api.nvim_create_user_command("ZenmodeCloseAll", function()
 end, { nargs = "?" })
 
 vim.api.nvim_create_user_command("ZenmodeOpenAll", function(c)
+	zenmode.zenmode_close_all()
 	zenmode.zenmode_open_all(c.fargs[1])
 end, { nargs = "?" })
