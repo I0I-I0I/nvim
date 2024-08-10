d248a3e6cacb3bf0f80fb0d1c99aab26f4218b6b
I0I-I0I <azudjndj@gmail.com>
2024-08-10 14:02:55 +0300

    Removed nvim-spider plugin, added neowords.nvim

--- lua/mini/plugins/utils/spider.lua
+++ lua/mini/plugins/utils/spider.lua
@@ -1,13 +1,28 @@
 local M = {
-	"chrisgrieser/nvim-spider",
+	"backdround/neowords.nvim",
 }
 
-M.opts = {}
+M.keys = function()
+	local neowords = require("neowords")
+	local p = neowords.pattern_presets
+	local subword_hops = neowords.get_word_hops(
+		p.snake_case,
+		p.camel_case,
+		p.upper_case,
+		p.number,
+		p.hex_color,
+		"\\v\\.\\.",
+		'\\v"[ ]*"',
+		"\\v[{(\\[]$",
+		"\\v[})\\]]$"
+	)
 
-M.keys = {
-	{ "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" }, desc = "w" },
-	{ "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" }, desc = "e" },
-	{ "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" }, desc = "b" },
-}
+	return {
+		{ "w", subword_hops.forward_start, mode = { "n", "x", "o" } },
+		{ "e", subword_hops.forward_end, mode = { "n", "x", "o" } },
+		{ "b", subword_hops.backward_start, mode = { "n", "x", "o" } },
+		{ "ge", subword_hops.backward_end, mode = { "n", "x", "o" } },
+	}
+end
 
 return M
