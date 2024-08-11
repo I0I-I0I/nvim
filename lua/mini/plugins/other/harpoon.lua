71d64f47ad11ac0ea3abd4cf03abc1857e914ee3
I0I-I0I <azudjndj@gmail.com>
2024-08-11 14:57:15 +0300

    Added harpoon

---
+++ lua/mini/plugins/other/harpoon.lua
@@ -0,0 +1,84 @@
+local M = {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+}
+
+function M.config()
+	local harpoon = require("harpoon")
+
+	-- REQUIRED
+	harpoon:setup()
+	-- REQUIRED
+end
+
+M.keys = function()
+	local harpoon = require("harpoon")
+
+	return {
+		{
+			"<leader>a",
+			function()
+				harpoon:list():add()
+			end,
+			desc = "Add file (Harpoon)",
+		},
+		{
+			"<C-m>",
+			function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end,
+			desc = "Open list of files (Harpoon)",
+		},
+		{
+			"<C-s>",
+			function()
+				harpoon:list():select(1)
+			end,
+			desc = "Select 1 (Harpoon)",
+		},
+		{
+			"<C-h>",
+			function()
+				harpoon:list():select(2)
+			end,
+			desc = "Select 2 (Harpoon)",
+		},
+		{
+			"<C-t>",
+			function()
+				harpoon:list():select(3)
+			end,
+			desc = "Select 3 (Harpoon)",
+		},
+		{
+			"<C-n>",
+			function()
+				harpoon:list():select(4)
+			end,
+			desc = "Select 4 (Harpoon)",
+		},
+		{
+			"<C-p>",
+			function()
+				harpoon:list():select(5)
+			end,
+			desc = "Select 5 (Harpoon)",
+		},
+		{
+			"<C-j>",
+			function()
+				harpoon:list():prev()
+			end,
+			desc = "Go to Previous file (Harpoon)",
+		},
+		{
+			"<C-k>",
+			function()
+				harpoon:list():next()
+			end,
+			desc = "Go to Next file (Harpoon)",
+		},
+	}
+end
+
+return M
