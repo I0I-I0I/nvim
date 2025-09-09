c3a50f57e781da35779758608f49ffc9030665e1
I0I-I0I <azudjndj@gmail.com>
2025-09-09 07:08:23 +0300

    add: make

---
+++ lua/make.lua
@@ -0,0 +1,22 @@
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "htmldjango" },
+    callback = function(args)
+        vim.bo[args.buf].makeprg = "djlint --lint --reformat --quiet %"
+    end
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "python" },
+    callback = function(args)
+        vim.bo[args.buf].makeprg = "ruff format %"
+    end
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
+    callback = function(args)
+        vim.bo[args.buf].makeprg = "eslint --fix %"
+    end
+})
+
+vim.keymap.set("n", "<leader>m", "<cmd>make<cr>")
