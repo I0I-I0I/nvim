2d588e90a340888bf0d9dc4314815b9fb3898eef
I0I-I0I <azudjndj@gmail.com>
2025-09-26 00:10:48 +0300

    feat: add supermaven ai completion

--- lua/make.lua
+++ lua/make.lua
@@ -22,14 +22,16 @@ vim.api.nvim_create_autocmd("FileType", {
     pattern = { "python" },
     callback = function(args)
         -- Lint the current file with :make
-        vim.bo[args.buf].makeprg = "ruff check --output-format=pylint"
+        vim.bo[args.buf].makeprg = "uv run ruff check --output-format=pylint"
         vim.bo[args.buf].errorformat = "%f:%l: %m"
         -- Format selection with 'gq'
-        vim.bo[args.buf].formatprg = "ruff format --stdin-filename %"
+        vim.bo[args.buf].formatprg = "uv run ruff format --stdin-filename %"
         vim.bo[args.buf].formatexpr = ""
-        vim.keymap.set("n", "<leader>f", "<cmd>w<cr><cmd>!ruff format %<cr>",
+        vim.keymap.set("n", "<leader>f", "<cmd>w<cr><cmd>!uv run ruff format %<cr>",
             { silent = true })
-        vim.keymap.set("n", "<leader>M", "<cmd>w<cr><cmd>silent !ruff check --fix % >/dev/null<cr>",
+        vim.keymap.set("n", "<leader>F", "<cmd>w<cr><cmd>!uv run ruff format<cr>",
+            { silent = true })
+        vim.keymap.set("n", "<leader>M", "<cmd>w<cr><cmd>silent !uv run ruff check --fix % >/dev/null<cr>",
             { silent = true })
     end })
 
