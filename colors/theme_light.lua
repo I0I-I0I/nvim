e412b729cdec9b6fd96c0687bafbc6e7b61043d3
I0I-I0I <azudjndj@gmail.com>
2026-03-30 03:00:30 +0300

    small improvements

--- colors/theme_light.lua
+++ colors/theme_light.lua
@@ -11,12 +11,12 @@ require("theme.init").apply({
         string = "#4d7f57",
         cursorline = "#f0ede5",
 
-        git_add = "#d9eadf",
+        git_add = "#5f8f6b",
         git_add_bg = "#d9eadf",
-        git_delete = "#d89e9e",
+        git_delete = "#9a4f4f",
         git_delete_bg = "#d89e9e",
-        git_change = "#d7d48d",
+        git_change = "#8c7a2b",
         git_change_bg = "#d7d48d",
-        git_text_bg = "#faf7f0",
+        git_text_bg = "#e6dfbf",
     },
 })
